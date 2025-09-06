e02ceddde008d85132b50b1a6d1c89a77ad58c80
louhaojie99 <851681631@qq.com>
2025-09-06 08:59:18 +0800

    feat: useWillUnmount hook add tips

--- src/hooks/useWillUnmount.ts
+++ src/hooks/useWillUnmount.ts
@@ -1,5 +1,9 @@
 import { useEffect } from 'react';
 
+/**
+ * 组件卸载生命周期钩子
+ * @param callback 卸载时执行的回调函数（无参数，无返回值）
+ */
 export const useWillUnmount = (callback: () => void) => {
   useEffect(() => {
     if (typeof callback !== 'function') return;
